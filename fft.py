d8b5d6c1fa51958d85a5a6ec7d8418114d0a4144
gzshao <petersgz@gmail.com>
2016-10-21 14:55:49 -0700

    Add files via upload

--- fft.py
+++ fft.py
@@ -6,10 +6,16 @@ from sympy import re, im, I
 from sympy.utilities.lambdify import lambdify
 import matplotlib.pyplot as plt
 
-# Fast Fourier Transform
+#Fourier Transform
 
-n = 1024
-s = np.random.random(n)
+n = 1000
+#noise = np.random.random(n)
+#count = np.arange(1024)
+count=np.arange(0,1,1.0/1300)
+print(count.shape[0])
+signal = np.sin(2*20*pi*count)
+s= signal #+noise
+#signal = np.sin()
 '''
 x = np.arange(n)
 
@@ -34,23 +40,27 @@ def DFT_slow(x):
     """Compute the discrete Fourier Transform of the 1D array x"""
     x = np.asarray(x, dtype=float)
     N = x.shape[0]
-    n = np.arange(N)
+    #n = np.arange(N)  # n = np.arange(1,N+1,1)   # from 1 to N, rather than from 0 to N-1
+    n = np.arange(1,N+1,1)
     k = n.reshape((N, 1))
     M = np.exp(-2j * np.pi * k * n / N)
-    return np.dot(M, x)
+    y = np.exp(x)
+    return np.dot(M, y)
 
 
-x=DFT_slow(s)
+xp=DFT_slow(s)
+x=np.absolute(xp)
+#x=np.real(xp)
 
-
-print(np.allclose(x,np.fft.fft(s)))
-print(x[:5])
+#print(np.allclose(x,np.fft.fft(s)))
+#print(s[:10])
 
 #plt.subplot(211)
 #plt.plot(s)
 #plt.subplot(212)
 #plt.xlim(-10,1000)
 #plt.plot(x)
-plt.ylim(-500,500)
-plt.plot(x,'g^',s,'r--')
+#plt.ylim(-5,5)
+#plt.plot(x,'g^',s,'r--')
+plt.plot(x)
 plt.show()
